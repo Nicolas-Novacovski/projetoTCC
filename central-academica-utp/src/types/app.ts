de9051c0e9bd327778d3c6afc4c537b7e1de6276
Thiagowx454 <thiago.ribeiro2@utp.edu.br>
2026-05-20 10:13:39 -0300

    Atualizando Tipagem

--- central-academica-utp/src/types/app.ts
+++ central-academica-utp/src/types/app.ts
@@ -30,6 +30,9 @@ export type ModerationPost = {
   author: string
   status: PostStatus
   submittedAt: string
+  description: string
+  contactEmail: string
+  location: string
 }
 
 export type RideOffer = {
